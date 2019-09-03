9c0b8ac5e79df29e86c08f7b51cb3aa4bfb602ae
Kat <katrina@madetech.com>
2019-09-03 15:53:26 +0100

    First commit

---
+++ lib/word_wrap_gem_exercise.rb
@@ -0,0 +1,6 @@
+require "word_wrap_gem_exercise/version"
+
+module WordWrapGemExercise
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/word_wrap_gem_exercise/version.rb
@@ -0,0 +1,3 @@
+module WordWrapGemExercise
+  VERSION = "0.1.0"
+end
